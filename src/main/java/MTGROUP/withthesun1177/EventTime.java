033760926448a41c4c150bbe297f398df433b801
MyPC <MyPC@MAYTINH-649U6L7>
2017-07-30 21:56:15 +0700

    added 1.EventTime.java 2.insertMysql.java 3.pom.xml

---
+++ src/main/java/MTGROUP/withthesun1177/EventTime.java
@@ -0,0 +1,26 @@
+package MTGROUP.withthesun1177;
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+public class EventTime{
+	public static void main(String[] args) throws IOException {
+		//get event time
+		Document document = Jsoup.connect("https://www.dailyfx.com/calendar").get();
+		org.jsoup.nodes.Element table = document.getElementById("daily-cal5");
+		System.out.println(table.text());
+		//detail extraction by regular expression
+		Pattern p = Pattern.compile("[0-9]{2}:[0-9]{2}");
+		Matcher m = p.matcher(table.text());
+		//insert the data into Mysql
+		insertMysql intoMysql = new insertMysql();
+		while (m.find()) {
+			System.out.println("same to : " + m.group());
+			intoMysql.insertData(m.group());
+		}
+	}
+
+}
---
+++ src/main/java/MTGROUP/withthesun1177/insertMysql.java
@@ -0,0 +1,25 @@
+package MTGROUP.withthesun1177;
+import java.sql.*;
+public class insertMysql{    
+	public void insertData(String eventTime) {
+		try {
+            // MySQLデータベースに接続 (DB名,ID,パスワードを指定)
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/event", "root", "a12345678");
+            System.out.println("MySQLに接続できました。");
+            System.out.println("received data is ="+ eventTime );
+            // ステートメントを作成
+            Statement stmt = con.createStatement();
+
+            // INSERT
+            stmt.executeUpdate("INSERT INTO economic (time) VALUES ('"+eventTime+"')");
+            System.out.println("Table economic　の timeカラムにinsert出来ました。");
+            // ステートメントをクローズ
+            stmt.close();
+            // 接続をクローズ
+            con.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+}

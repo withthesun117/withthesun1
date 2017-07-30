d710acdaa1a786b0fa1444472902701f6c391fe0
MyPC <MyPC@MAYTINH-649U6L7>
2017-07-30 21:40:09 +0700

    test commit

---
+++ src/main/java/MTGROUP/withthesun1177/App.java
@@ -0,0 +1,13 @@
+package MTGROUP.withthesun1177;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
